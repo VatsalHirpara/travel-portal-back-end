dc6dbf30cde7009aff4839b60de34c130f2f8c6b
Vatsal Hirpara <vatsalplay9@gmail.com>
2020-04-28 19:24:05 +0530

    add main class

--- src/main/java/com/nagarro/training/travelportal/TravelportalApplication.java
+++ src/main/java/com/nagarro/training/travelportal/TravelportalApplication.java
@@ -4,21 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class TravelportalApplication {
 	
-//	@Autowired
-//	UserRepository userRepository; 
-//	
-//	@PostConstruct
-//	public void init() {  
-//		List<User> l = new ArrayList<>();
-//		User u1 = new User(1,"vatsal","hirpara");u1.setFirstName("vatsal");l.add(u1);
-//		userRepository.saveAll(l);  
-//	}
-//	
 	public static void main(String[] args) {
 		SpringApplication.run(TravelportalApplication.class, args);
-		System.out.println("running!");
+		System.out.println("App is running!");
 	}
 }
