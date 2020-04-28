06f0e3676e3cc6e862721599ece1eea7e7ce18d7
Vatsal Hirpara <vatsalplay9@gmail.com>
2020-04-28 23:36:39 +0530

    change @PathParam to @PathVariable

--- src/main/java/com/nagarro/training/travelportal/resource/TicketResource.java
+++ src/main/java/com/nagarro/training/travelportal/resource/TicketResource.java
@@ -2,10 +2,9 @@ package com.nagarro.training.travelportal.resource;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,23 +14,23 @@ import com.nagarro.training.travelportal.repository.TicketRepository;
 
 @RestController
 public class TicketResource {
-	
+
 	@Autowired
 	TicketRepository ticketRepository;
-	
+
 	@GetMapping("/tickets")
-	public List<Ticket> getTickets(){
+	public List<Ticket> getTickets() {
 		return ticketRepository.findAll();
 	}
-	
+
+	@GetMapping("/users/{id}/tickets")
+	public List<Ticket> getTicketsById(@PathVariable(value = "id") Integer id) {
+		return ticketRepository.findByUserId(id);
+	}
+
 	@PostMapping("/tickets")
 	public Ticket addTicket(@RequestBody Ticket ticket) {
 		return ticketRepository.save(ticket);
 	}
-	
-	
-	@GetMapping("/users/{id}/tickets")
-	public List<Ticket> getTicketsById(@PathParam(value = "id") int id){
-		return ticketRepository.findByUser_Id(id);
-	}
+
 }
