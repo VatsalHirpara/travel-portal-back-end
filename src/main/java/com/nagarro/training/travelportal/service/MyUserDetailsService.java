39bef370ece862f87689944e8f23c7947883aae9
Vatsal Hirpara <vatsalplay9@gmail.com>
2020-04-28 19:26:59 +0530

    add MyUserDetailsService

---
+++ src/main/java/com/nagarro/training/travelportal/service/MyUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.nagarro.training.travelportal.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.nagarro.training.travelportal.model.MyUserDetails;
+import com.nagarro.training.travelportal.model.User;
+import com.nagarro.training.travelportal.repository.UserRepository;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService{
+
+	@Autowired
+    UserRepository userRepository;
+	
+	@Override
+	public MyUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		Optional<User> user = userRepository.findByEmail(email);
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + email));
+        return  user.map(MyUserDetails::new).get();
+	}
+}
