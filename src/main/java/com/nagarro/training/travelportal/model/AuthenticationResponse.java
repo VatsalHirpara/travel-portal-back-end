54b8b88345e820626fa833a7d462a417b706222d
Vatsal Hirpara <vatsalplay9@gmail.com>
2020-05-02 21:19:50 +0530

    add isAdmin field

--- src/main/java/com/nagarro/training/travelportal/model/AuthenticationResponse.java
+++ src/main/java/com/nagarro/training/travelportal/model/AuthenticationResponse.java
@@ -3,13 +3,15 @@ package com.nagarro.training.travelportal.model;
 public class AuthenticationResponse {
 	private final String jwt;
 	private final int userId;
+	private final boolean isAdmin;
 	private final int status;
 	
 
-	public AuthenticationResponse(String jwt,int userId,int status) {
+	public AuthenticationResponse(String jwt,int userId,int status,boolean isAdmin) {
         this.jwt = jwt;
         this.userId=userId;
         this.status=status;
+        this.isAdmin=isAdmin;
     }
 
     public String getJwt() {
@@ -20,5 +22,8 @@ public class AuthenticationResponse {
     }
     public int getStatus() {
     	return this.status;
+    }
+    public boolean isAdmin() {
+    	return isAdmin;
     }
 }
--- src/main/java/com/nagarro/training/travelportal/model/MyUserDetails.java
+++ src/main/java/com/nagarro/training/travelportal/model/MyUserDetails.java
@@ -8,64 +8,69 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public class MyUserDetails implements UserDetails {
 
-	@Override
-	public String toString() {
-		return "MyUserDetails [userName=" + userName + ", password=" + password  + "]";
-	}
-
 	private static final long serialVersionUID = -1410602661036610351L;
 	private int id;
 
 	private String userName;
-    private String password;
+	private String password;
+	private boolean isAdmin;
 //    private List<GrantedAuthority> authorities;
 
-    public MyUserDetails(User user) {
-        this.userName = user.getEmail();
-        this.password = user.getPassword();
-        this.id = user.getId();
+	public boolean getIsAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	public MyUserDetails(User user) {
+		this.userName = user.getEmail();
+		this.password = user.getPassword();
+		this.id = user.getId();
+		this.isAdmin = user.isAdmin();
 //        this.authorities = Arrays.stream(user.getRoles().split(","))
 //                    .map(SimpleGrantedAuthority::new)
 //                    .collect(Collectors.toList());
-    }
+	}
 
-    public int getId() {
-    	return id;
-    }
-    
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
 //        return authorities;
-    	return new ArrayList<>();
-    }
+		return new ArrayList<>();
+	}
 
-    @Override
-    public String getPassword() {
-        return password;
-    }
+	@Override
+	public String getPassword() {
+		return password;
+	}
 
-    @Override
-    public String getUsername() {
-        return userName;
-    }
+	@Override
+	public String getUsername() {
+		return userName;
+	}
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
