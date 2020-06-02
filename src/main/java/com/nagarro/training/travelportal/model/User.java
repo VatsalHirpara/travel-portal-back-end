83927d450e8bd9909fe5163a94ad8bbad9406897
Vatsal Hirpara <vatsalplay9@gmail.com>
2020-06-02 22:10:10 +0530

    add nullable = false to user model

--- src/main/java/com/nagarro/training/travelportal/model/User.java
+++ src/main/java/com/nagarro/training/travelportal/model/User.java
@@ -3,13 +3,13 @@ package com.nagarro.training.travelportal.model;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,26 +23,49 @@ public class User {
 	@Id
 	@GeneratedValue
 	private Integer id;
+
+	@Column(nullable = false)
 	private String firstName;
+
+	@Column(nullable = false)
 	private String lastName;
+
+	@Column(nullable = false)
 	private String businessUnit;
+
+	@Column(nullable = false)
 	private String title;
+
+	@Column(unique = true, nullable = false)
 	private String email;
+
+	@Column(nullable = false)
 	private String telephone;
+
+	@Column(nullable = false)
 	private String address1;
+
+	@Column(nullable = true)
 	private String address2;
+
+	@Column(nullable = false)
 	private String city;
+
+	@Column(nullable = false)
 	private String state;
+
+	@Column(nullable = false)
 	private String zip;
+
+	@Column(nullable = false)
 	private String country;
-	
+
 	private boolean isAdmin;
-    private String roles;
-    
+
 	@JsonIgnore
 	private String password;
-	
+
 	@JsonIgnore
-	@OneToMany(mappedBy = "user",cascade=CascadeType.ALL)
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 	private List<Ticket> tickets;
 }
