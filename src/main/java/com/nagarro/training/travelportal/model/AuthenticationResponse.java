9063908b6edfed38ee2c3cd99e770138c4e87050
Vatsal Hirpara <vatsalplay9@gmail.com>
2020-06-02 22:13:29 +0530

    formatting

--- src/main/java/com/nagarro/training/travelportal/model/AuthenticationResponse.java
+++ src/main/java/com/nagarro/training/travelportal/model/AuthenticationResponse.java
@@ -5,25 +5,27 @@ public class AuthenticationResponse {
 	private final int userId;
 	private final boolean isAdmin;
 	private final int status;
-	
 
-	public AuthenticationResponse(String jwt,int userId,int status,boolean isAdmin) {
-        this.jwt = jwt;
-        this.userId=userId;
-        this.status=status;
-        this.isAdmin=isAdmin;
-    }
+	public AuthenticationResponse(String jwt, int userId, int status, boolean isAdmin) {
+		this.jwt = jwt;
+		this.userId = userId;
+		this.status = status;
+		this.isAdmin = isAdmin;
+	}
 
-    public String getJwt() {
-        return this.jwt;
-    }
-    public int getUserId() {
-    	return this.userId;
-    }
-    public int getStatus() {
-    	return this.status;
-    }
-    public boolean isAdmin() {
-    	return isAdmin;
-    }
+	public String getJwt() {
+		return this.jwt;
+	}
+
+	public int getUserId() {
+		return this.userId;
+	}
+
+	public int getStatus() {
+		return this.status;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
 }
