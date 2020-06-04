d8608deeddd2ee45bbcb94c0aa92df697e35c9bb
Vatsal Hirpara <vatsalplay9@gmail.com>
2020-06-04 12:00:02 +0530

    remove debug print statment

--- src/main/java/com/nagarro/training/travelportal/TravelportalApplication.java
+++ src/main/java/com/nagarro/training/travelportal/TravelportalApplication.java
@@ -2,13 +2,11 @@ package com.nagarro.training.travelportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class TravelportalApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(TravelportalApplication.class, args);
-		System.out.println("App is running!");
 	}
 }
