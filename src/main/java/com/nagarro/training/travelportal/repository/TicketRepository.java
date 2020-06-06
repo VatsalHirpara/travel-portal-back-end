d5101e16d7aa49ca908f869b6f5ccaa573763e5b
Vatsal Hirpara <vatsalplay9@gmail.com>
2020-06-06 12:44:11 +0530

    add paging support for ticket repo method findByUserId

--- src/main/java/com/nagarro/training/travelportal/repository/TicketRepository.java
+++ src/main/java/com/nagarro/training/travelportal/repository/TicketRepository.java
@@ -1,13 +1,15 @@
 package com.nagarro.training.travelportal.repository;
 
 import java.util.List;
-import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.nagarro.training.travelportal.model.Ticket;
-import com.nagarro.training.travelportal.model.User;
-public interface TicketRepository extends JpaRepository<Ticket,Integer>{
+
+public interface TicketRepository extends JpaRepository<Ticket, Integer> {
 	public List<Ticket> findByUserId(Integer id);
-} 
+
+	public Page<Ticket> findByUserId(Integer id, Pageable pageable);
+}
