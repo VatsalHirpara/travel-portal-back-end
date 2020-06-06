81124baa24217e6cd7f9401532ec7a2b8bc81267
Vatsal Hirpara <vatsalplay9@gmail.com>
2020-06-06 12:45:02 +0530

    add @Transactional to service methods

--- src/main/java/com/nagarro/training/travelportal/service/MailSenderService.java
+++ src/main/java/com/nagarro/training/travelportal/service/MailSenderService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MailSenderService {
@@ -11,6 +12,7 @@ public class MailSenderService {
 	@Autowired
 	private JavaMailSender javaMailSender;
 
+	@Transactional
 	public void sendEmail(String email, String password) {
 		SimpleMailMessage msg = new SimpleMailMessage();
 		msg.setTo(email);
--- src/main/java/com/nagarro/training/travelportal/service/MyUserDetailsService.java
+++ src/main/java/com/nagarro/training/travelportal/service/MyUserDetailsService.java
@@ -3,26 +3,27 @@ package com.nagarro.training.travelportal.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nagarro.training.travelportal.model.MyUserDetails;
 import com.nagarro.training.travelportal.model.User;
 import com.nagarro.training.travelportal.repository.UserRepository;
 
 @Service
-public class MyUserDetailsService implements UserDetailsService{
+public class MyUserDetailsService implements UserDetailsService {
 
 	@Autowired
-    UserRepository userRepository;
-	
+	UserRepository userRepository;
+
 	@Override
+	@Transactional
 	public MyUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		
+
 		Optional<User> user = userRepository.findByEmail(email);
-        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + email));
-        return  user.map(MyUserDetails::new).get();
+		user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + email));
+		return user.map(MyUserDetails::new).get();
 	}
 }
